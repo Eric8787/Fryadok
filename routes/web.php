5152d7235fa7c4f8ae14c7e3662d07e02c220c08
nicolasandrada <nicolas.andrada@hotmail.com>
2021-10-22 23:55:01 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\RegisterController;
-use app\Http\Controllers\SessionsController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 
 Route::get('/', function () {
     return view('home');
